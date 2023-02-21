f07badaa78228bedd3c91bd87473481986d525e2
SofiaVga <sofiavegagomez04@gmail.com>
2023-02-21 06:00:51 -0600

    Subiendo Archivo

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String [] args){
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce tu nombre: ");
+        String nombre = teclado.nextLine();
+
+        System.out.println("Introduce tu profesion: ");
+        String profesion = teclado.nextLine();
+
+        System.out.println("Introduce tu nacionalidad: ");
+        String nacionalidad = teclado.nextLine();
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Profesion: " + profesion);
+        System.out.println("Nacionalidad: " + nacionalidad);
+
+
+
     }
 }
